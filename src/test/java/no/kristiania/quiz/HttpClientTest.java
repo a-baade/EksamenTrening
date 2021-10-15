afcf11828bf419dbb22945bacb59d8eca6647e99
a-baade <andreasbaade@hotmail.com>
2021-10-15 11:36:06 +0200

    test should return 404 status-code (🔴)

--- src/test/java/no/kristiania/quiz/HttpClientTest.java
+++ src/test/java/no/kristiania/quiz/HttpClientTest.java
@@ -8,8 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
     @Test
-    public void returnStatusCode() throws IOException {
+    public void shouldReturnStatusCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org",80,"/html");
         assertEquals(200, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturn404StatusCode() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org",80,"/this-page-does-not-exist");
+        assertEquals(404,client.getStatusCode());
     }
 }
