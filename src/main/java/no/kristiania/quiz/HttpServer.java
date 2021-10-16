c15f4538103143ffa693febf2bc86bb0bbf264dd
a-baade <andreasbaade@hotmail.com>
2021-10-16 16:14:54 +0200

    test should respond with 404 for unknown request target (🔴)

--- src/main/java/no/kristiania/quiz/HttpServer.java
+++ src/main/java/no/kristiania/quiz/HttpServer.java
@@ -3,7 +3,7 @@ package no.kristiania.quiz;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+
 
 
 public class HttpServer {
@@ -29,9 +29,25 @@ public class HttpServer {
         String httpResponse = "HTTP/1.1 200 OK\r\n" +
                 "Content-Length: " + httpMessage.getBytes().length + "\r\n" +
                 "Connection: close\r\n" +
+                "Content-Type: text/html; charset=utf-8\r\n" +
                 "\r\n" +
                 httpMessage;
         clientSocket.getOutputStream().write(httpResponse.getBytes());
+
+        new Thread(this::handleConnections).start();
     }
 
+    private void handleConnections() {
+        try (Socket clientSocket = serverSocket.accept())
+        {
+            String response404 = "HTTP/1.1 404 NotFound\r\nContent-Length: 0\r\n\r\n";
+            clientSocket.getOutputStream().write(response404.getBytes());
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getPort() {
+        return serverSocket.getLocalPort();
+    }
 }
--- src/test/java/no/kristiania/quiz/HttpServerTest.java
+++ src/test/java/no/kristiania/quiz/HttpServerTest.java
@@ -8,5 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
 
+    private final HttpServer server = new HttpServer(0);
 
+    public HttpServerTest() throws IOException {
+    }
+
+    @Test
+    void shouldReturn404ForUnknownRequest() throws IOException {
+        HttpClient client = new HttpClient("localhost",server.getPort(), "/non-existing");
+        assertEquals(404,client.getStatusCode());
+    }
 }
